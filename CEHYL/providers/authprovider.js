131247a94badb83efe38b066d34b5d730e2eab94
Exeexe93 <e0318484@u.nus.edu>
2020-06-01 12:09:41 +0800

    Adding authentication flows

---
+++ CEHYL/providers/authprovider.js
@@ -0,0 +1,35 @@
+import React, {createContext} from 'react';
+import dispatch from '../App';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({children, dispatch}) => {
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+      signOut: () => dispatch({type: 'SIGN_OUT'}),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+    }),
+    [],
+  );
+
+  return (
+    <AuthContext.Provider value={authContext}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ CEHYL/screens/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button, View, Text} from 'react-native';
+import {AuthContext} from '../providers/authprovider';
+
+function HomeScreen({navigation}) {
+  const {signOut} = React.useContext(AuthContext);
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button title="Go back to Sign in" onPress={() => signOut()} />
+    </View>
+  );
+}
+
+export default HomeScreen;
