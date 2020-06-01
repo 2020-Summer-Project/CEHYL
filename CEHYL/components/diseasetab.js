fcc912dbc9f0472f462310000e57db008ccc1e20
khsc96 <khsc96@gmail.com>
2020-06-01 18:24:56 +0800

    Fix minor bug

--- CEHYL/components/diseasetab.js
+++ CEHYL/components/diseasetab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { Text, View } from 'react-native';
 
 
 export const DiseaseTab = (props) => {
