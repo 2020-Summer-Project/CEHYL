b41c84b74db3672469d211a32112f6980735af71
khsc96 <khsc96@gmail.com>
2020-06-01 19:43:45 +0800

    Integrate API with front-end

--- CEHYL/components/diseasetab.js
+++ CEHYL/components/diseasetab.js
@@ -3,8 +3,10 @@ import { Text, View } from 'react-native';
 
 
 export const DiseaseTab = (props) => {
+    console.log(props.dateRange + "hihi")
     return(
         <View>
+            <Text>{props.dateRange}</Text>
             <Text>Hi from tab {props.week}</Text>
         </View>
     );
--- CEHYL/screens/disease.js
+++ CEHYL/screens/disease.js
@@ -2,13 +2,33 @@ import React, { useState } from 'react';
 import {StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import {DiseaseTab} from '../components/diseasetab';
 
+import * as DiseaseAPI from '../API/disease';
+
+const getLatestWeek = () => {
+  return DiseaseAPI.getLatestEpiWeekNum()
+}
 
 
 function DiseaseScreen() {
   const [screen, setScreen] = useState(<DiseaseTab week="1"/>);
+  const [previousFourWeeks, setPreviousFourWeeks] = useState(["", "", "", ""])
+
+  if (!previousFourWeeks[0]) {
+    getLatestWeek().then(
+      (epi_week) => {
+        setPreviousFourWeeks(DiseaseAPI.getPreviousFourWeeks(epi_week).map((week) => week.replace("-W", " Week ")))
+      } 
+    )
+  }
+
 
   const pressButton = (key) => {
-    setScreen(<DiseaseTab week={key}></DiseaseTab>)
+    const index = parseInt(key) - 1
+    const length = previousFourWeeks[index].length;
+    const year = parseInt(previousFourWeeks[index].substring(0, 4))
+    const week = parseInt(previousFourWeeks[index].substring(length - 2, length))
+    setScreen(<DiseaseTab week={key} dateRange={DiseaseAPI.getDateRangeOfWeek(week, year)}></DiseaseTab>)
+    console.log(DiseaseAPI.getDateRangeOfWeek(week, year))
   }
 
   return (
@@ -16,16 +36,16 @@ function DiseaseScreen() {
       <Text style={styles.header}>Weekly Disease</Text>
       <View style={styles.buttonsContainer}>
         <TouchableOpacity style={styles.button} onPress={() => pressButton("1")}>
-          <Text>Week 1</Text>
+          <Text style={styles.textStyle}>{previousFourWeeks[0]}</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={() => pressButton("2")}>
-          <Text>Week 2</Text>
+          <Text style={styles.textStyle}>{previousFourWeeks[1]}</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={() => pressButton("3")}>
-          <Text>Week 3</Text>
+          <Text style={styles.textStyle}>{previousFourWeeks[2]}</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={() => pressButton("4")}>
-          <Text>Week 4</Text>
+          <Text style={styles.textStyle}>{previousFourWeeks[3]}</Text>
         </TouchableOpacity>
       </View>
       {screen}
@@ -58,6 +78,9 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 32,
     fontWeight: 'bold',
+  },
+  textStyle: {
+    textAlign: 'center',
   }
 })
 
