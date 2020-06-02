281a0263770da3e0aedf255a65767cdfd6f62ad8
khsc96 <khsc96@gmail.com>
2020-06-03 00:16:26 +0800

    Add listener for firebase data retrieving

--- CEHYL/screens/temperature.js
+++ CEHYL/screens/temperature.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Image, Text, TouchableOpacity, View, Alert} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, FlatList, Text, TouchableOpacity, View, Alert} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 import { styles } from '../styles/temperaturestyle'
 import { TextInput } from 'react-native-gesture-handler';
 import Card from '../components/card';
+import firebase from '../Firebase';
+import {v4 as uuidv4} from 'uuid';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
-
-
-const addTemperature = (temp) => {
-    return !isNaN(temp) ? console.log(temp) : Alert.alert('Not a number')
-}
 
 export default function TemperatureScreen({ navigation }) {
     const [temperature, setTemperature] = useState('');
@@ -18,7 +15,43 @@ export default function TemperatureScreen({ navigation }) {
     const [date, setDate] = useState(new Date());
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
+    const [listOfTemperature, setListOfTemperature] = useState([])
   
+    const user = firebase.auth().currentUser;
+
+    const addTemperature = () => {
+        if (isNaN(temperature)) {
+            return Alert.alert('Not a number')
+        }
+        console.log(temperature)
+        addTemperatureToDatabase()
+
+        setTemperature('')
+        setDate(new Date())
+    }
+
+    const addTemperatureToDatabase = () => {
+        const dateTime = dateTimeInString()
+        console.log(dateTime)
+        firebase.database().ref('temperature/' + user.uid + '/' + dateTime).set({
+            temperature: temperature,
+            date: dateTime,
+        })
+    }
+
+    const dateTimeInString = () => {
+        let minutes = formatMinutes(date.getMinutes().toString())
+        console.log(minutes)
+        return date.getDate() + "-" + (date.getMonth() + 1) + '-' + date.getFullYear() + " " + date.getHours() + ":" + minutes
+    }
+
+    const formatMinutes = (minutes) => {
+        if (minutes.length === 2) {
+            return minutes
+        }
+        return "0" + minutes
+    }
+
     const onChange = (event, selectedDate) => {
       const currentDate = selectedDate || date;
       setShow(Platform.OS === 'ios');
@@ -38,16 +71,55 @@ export default function TemperatureScreen({ navigation }) {
       showMode('time');
     };
 
+    const processRetrievedData = (dataRetrieved) => {
+        const sortedData = {}
+        Object.keys(dataRetrieved).sort(
+            (a, b) => {
+                return new Date(a) > new Date(b) ? 1 : -1
+            })
+            .reverse()
+            .forEach((key) => sortedData[key] = dataRetrieved[key])
+        const result = Object.values(sortedData)
+            .map(data => {
+                return {...data, id: uuidv4()}
+            })
+            console.log(result)
+        return result
+    }
+
+    useEffect(() =>{
+        try {
+            const listener = firebase.database().ref('temperature/' + user.uid)
+            listener.on('value', function(snapshot) {
+                const dataRetrieved = snapshot.val();
+                const result = processRetrievedData(dataRetrieved);
+                setListOfTemperature(result)
+            })
+                // .once('value')
+                // .then(function(snapshot) {
+                //     Object.keys(dataRetrieved).sort( (a, b) => {
+                //         return new Date(a) - new Date(b)
+                //     }).reverse().forEach((key) => sortedData[key] = dataRetrieved[key])
+                //     const result = Object.values(sortedData).map(data => {
+                //         return {...data, id: uuidv4()}
+                //     })
+                //     setListOfTemperature(result)
+                // });
+        } catch(err) {
+            Alert.alert(err);
+        }
+    }, [])
+
     return(
         <View style={styles.container}>
             <Text style={header}>Temperature Screen</Text>
 
             <View style={styles.row}>
                 <TouchableOpacity style={button} onPress={showDatepicker}>
-                    <Text style={styles.textCenter}>{date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear()}</Text>
+                    <Text style={styles.textCenter}>{date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={button} onPress={showTimepicker}>
-                    <Text style={styles.textCenter}>{date.getHours() + ":" + date.getMinutes()}</Text>
+                    <Text style={styles.textCenter}>{date.getHours() + ":" + formatMinutes(date.getMinutes().toString())}</Text>
                 </TouchableOpacity>
             </View>            
 
@@ -66,7 +138,8 @@ export default function TemperatureScreen({ navigation }) {
                 <TextInput 
                     keyboardType='numeric' style={styles.textInput} 
                     placeholder="Your temperature here"
-                    onChangeText={(temp) => setTemperature(temp)}
+                    onChangeText={setTemperature}
+                    value={temperature}
                 />
                 <TouchableOpacity onPress={() => {
                         addTemperature(temperature)
@@ -75,20 +148,18 @@ export default function TemperatureScreen({ navigation }) {
                 </TouchableOpacity>
             </View>
             <View>
-                {/* list of temperature records here */}
-                <Text></Text>
-                <Card>
-                    <Text style={{ paddingRight: 10}}>36.5</Text>
-                    <Text>20/5/2020</Text>
-                </Card>
-                <Card>
-                    <Text style={{ paddingRight: 10}}>36.5</Text>
-                    <Text>20/5/2020</Text>
-                </Card>
-                <Card>
-                    <Text style={{ paddingRight: 10}}>36.5</Text>
-                    <Text>20/5/2020</Text>
-                </Card>
+                <FlatList
+                    data={listOfTemperature}
+                    renderItem={({item}) => (
+                        <Card>
+                            <View style={styles.row}>
+                                <Text>{item.date}</Text>
+                                <Text>{item.temperature}</Text>
+                            </View>
+                        </Card>
+                    )}
+                    keyExtractor={item => item.id}
+                />
             </View>
         </View>
     );
